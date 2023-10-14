7ba50db69f770f6ad1efb2539b6367ed17dc9af3
OmMandlik02 <89374137+OmMandlik02@users.noreply.github.com>
2023-10-14 23:15:47 +0530

    Update mongoose.js

--- api/config/mongoose.js
+++ api/config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb+srv://classroomom3:Classroom@cluster0.dfqsnll.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://Ommandlik:Mandlik7878@cluster0.i3cjcqk.mongodb.net/?retryWrites=true&w=majority");
     console.log('Database connected successfully');
 }
 mongoose.connection.on("disconnected", function () {
